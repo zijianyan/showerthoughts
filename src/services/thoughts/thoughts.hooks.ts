be8a61eba34aaa89fa5bd8d6368e9701ab3ab612
Zijian Yan <zi@Zijians-MacBook-Air.local>
2022-01-29 21:04:40 -0500

    remove twit

--- src/services/thoughts/thoughts.hooks.ts
+++ src/services/thoughts/thoughts.hooks.ts
@@ -1,6 +1,5 @@
 import { HooksObject } from '@feathersjs/feathers';
 
-const Twit = require('twit')
 
 const credentials = {
   consumer_key:         process.env.TWITTER_API_KEY,
@@ -9,13 +8,11 @@ const credentials = {
   access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
 }
 
-const twitter = new Twit(credentials)
 
 const publish = async (context:any) => {
   const { data } = context
   if (data.publish === true) {
     console.log('publishing')
-    twitter.post('statuses/update', { status: `${data.text} ${Date.now()}`}, (err:any, data:any, response:any) => { console.log('posting to twitter. data:', data)})
   }
   return context;
 }

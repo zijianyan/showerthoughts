16cd57b13911e3b2509a6417cd779fdecf7b4711
Zijian Yan <zi@Zijians-MacBook-Air.local>
2022-01-30 20:18:28 -0500

    add validation hook from feathers-hooks-common

--- src/app.hooks.ts
+++ src/app.hooks.ts
@@ -1,14 +1,28 @@
 // Application hooks that run for every service
 // Don't remove this comment. It's needed to format import lines nicely.
 
+const { validate } = require('feathers-hooks-common')
+
+interface Thought {
+  text: string;
+  author: string;
+  publish: boolean;
+}
+
+const validateLength = (values:Thought, cb:any) => {
+  if (values.text.length > 280) {
+    throw new Error(`Thoughts must be 280 characters or less`)
+  }
+}
+
 export default {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
+    create: [validate(validateLength)],
+    update: [validate(validateLength)],
+    patch: [validate(validateLength)],
     remove: []
   },
 
